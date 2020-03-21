4e58912ac1ef72474b495656035cc94272bc5d6d
Ryan Menezes <ryanvmenezes@gmail.com>
2020-03-20 22:14:23 -0700

    updating filepaths

--- get-by-id.py
+++ get-by-id.py
@@ -9,7 +9,7 @@ import populartimes as pt
 api_key = 'AIzaSyA92zfr0K_Fya4-UTaiVuqlLMcjpMPXh2M'
 
 places = []
-with open('/home/ryan/repos/random/foottraffic/places.csv') as infile:
+with open('places.csv') as infile:
     dr = csv.DictReader(infile)
     for d in dr:
         places.append(d)
@@ -18,5 +18,5 @@ placeinfo = [pt.get_id(api_key, p['gmapsid']) for p in places]
 
 timestamp = datetime.datetime.now(tz=pytz.timezone('US/Pacific')).isoformat()
 
-with open(f'/home/ryan/repos/random/foottraffic/output/{timestamp}.json', 'w') as outfile:
+with open(f'output/{timestamp}.json', 'w') as outfile:
     outfile.write(json.dumps(placeinfo))
