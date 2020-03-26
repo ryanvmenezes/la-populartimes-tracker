2b3fef89a5d12385b64b71a9a0a22478ce9f7f46
Ryan Menezes <ryanvmenezes@gmail.com>
2020-03-25 21:08:05 -0700

    new organization

--- analysis/01_analysis.R
+++ analysis/01_analysis.R
@@ -1,6 +1,9 @@
+library(furrr)
 library(jsonlite)
 library(lubridate)
 library(tidyverse)
+
+plan(multisession)
 
 # raw data
 
@@ -39,15 +42,27 @@ currentpop
 
 # list of places being tracked
 
-places = currentpop %>% 
-  select(id, name) %>% 
+gmapsplaces = data %>% 
+  transmute(
+    placeinfo = map(
+      jsondata,
+      ~.x %>% 
+        as_tibble() %>% 
+        select(id, name, address, types, coordinates) %>% 
+        flatten()
+    )
+  ) %>% 
+  unnest(c(placeinfo)) %>% 
+  mutate(types = map_chr(types, str_c, collapse = '|')) %>% 
   distinct()
 
-places
+gmapsplaces  
+
+gmapsplaces %>% write_csv('clean/google-maps-place-info.csv')
 
 # popular times
 
-# cleaning up the data
+## cleaning up the data
 
 poptimesclean = data %>%
   transmute(
@@ -61,7 +76,7 @@ poptimesclean = data %>%
   ) %>% 
   unnest(c(poptimes)) %>% 
   mutate(
-    query = map(
+    query = future_map(
       populartimes,
       safely(
         ~.x %>% 
@@ -71,8 +86,9 @@ poptimesclean = data %>%
           pivot_longer(-name, names_to = 'hour', values_to = 'popularity') %>% 
           mutate(name = str_sub(name, end = 3)) %>% 
           unite(dayhour, name:hour) %>% 
-          pivot_wider(names_from = dayhour, values_from = popularity)
-      )
+          pivot_wider(names_from = dayhour, values_from = popularity),
+      ),
+      .progress = TRUE
     ),
     error = map_chr(query, ~typeof(pluck(., 'error'))),
     ptimebydayhour = map(query, ~pluck(., 'result'))
@@ -80,13 +96,13 @@ poptimesclean = data %>%
 
 poptimesclean
 
-# locations that don't consistently have the populartimes
+## locations that don't consistently have the populartimes
 
 poptimesclean %>%
   filter(error != 'NULL') %>% 
   count(name)
 
-# cleaned popular times
+## cleaned popular times
 
 poptimes = poptimesclean %>% 
   anti_join(
@@ -95,7 +111,6 @@ poptimes = poptimesclean %>%
       select(id, name) %>%
       distinct()
   ) %>% 
-  # filter(error == 'NULL') %>% 
   select(datadatetime, id, name, ptimebydayhour) %>% 
   unnest(c(ptimebydayhour)) %>% 
   arrange(id, name, datadatetime)
@@ -105,15 +120,25 @@ poptimes
 # how often are the popular times changing?
 # if any of these are n > 1, there's been a change in the popular times data
 
-change.in.pop = poptimes %>% 
-  distinct_at(vars(-datadatetime)) %>% 
+expected.by.location = poptimes %>% 
+  arrange(id, datadatetime) %>% 
+  distinct_at(vars(-datadatetime), .keep_all = TRUE)
+
+expected.by.location
+
+expected.by.location %>% 
+  write_csv('clean/expected-time-by-location.csv')
+
+change.in.pop = expected.by.location %>% 
   count(id, name) %>% 
   filter(n > 1)
 
 change.in.pop %>% 
-  write_csv('analysis/change-in-pop.csv')
+  write_csv('clean/change-in-pop.csv')
 
 # joining currentpopularity and exppopularity for that hour
+
+places = read_csv('places.csv')
 
 popularity.expected = currentpop %>% 
   right_join(
@@ -122,18 +147,24 @@ popularity.expected = currentpop %>%
       nest() %>% 
       ungroup()
   ) %>%
+  left_join(
+    places,
+    by = c('id' = 'gmapsid')
+  ) %>% 
   mutate(
     exppopularity = map2_chr(
       str_c(dayofweek, hourofday, sep = '_h'),
       data,
-      ~.y %>% 
-        pull(.x)
-    )
+      ~.y %>% pull(.x)
+    ),
+    exppopularity = as.integer(exppopularity)
   ) %>% 
-  select(datadatetime, id, name, current_popularity, dayofweek, hourofday, exppopularity) %>% 
-  mutate(exppopularity = as.integer(exppopularity))
+  select(datadatetime, venuename, typeofspace, current = current_popularity, expected = exppopularity)
 
 popularity.expected
 
+writetime = stamp('2020-10-20 23:30:47')
+
 popularity.expected %>% 
-  write_csv('analysis/popularity-expected.csv')
+  mutate(datadatetime = writetime(datadatetime)) %>% 
+  write_csv('clean/popularity-expected.csv')
--- analysis/02_plots.R
+++ analysis/02_plots.R
@@ -1,57 +1,26 @@
 library(tidyverse)
 library(lubridate)
 
-places = read_csv('places.csv')
-
-places
-
-popularity = read_csv('analysis/popularity-expected.csv') %>%
-  mutate(datadatetime = lubridate::as_datetime(datadatetime, tz = 'America/Los_Angeles')) %>% 
-  left_join(
-    places %>% select(venuename, typeofspace, id = gmapsid)
-  ) %>% 
-  select(datadatetime, venuename, typeofspace, current = current_popularity, expected = exppopularity) %>% 
-  pivot_longer(cols = c(current, expected), names_to = 'popularity.type', values_to = 'popularity') %>% 
-  arrange(venuename, datadatetime)
+popularity = read_csv('clean/popularity-expected.csv') %>%
+  mutate(datadatetime = floor_date(datadatetime, unit = 'hour')) %>% 
+  distinct(datadatetime, venuename, typeofspace, .keep_all = TRUE)
 
 popularity
 
-makeplot = function(data, title = '') {
-  ggplot(data, aes(x = datadatetime, y = popularity, color = popularity.type)) +
-    geom_line() +
-    facet_wrap(. ~ venuename) +
-    xlab('') +
-    ggtitle(title) +
-    theme_minimal() +
-    theme(axis.text.x = element_text(angle = 90))
-}
-
-comparison.chart = popularity %>% makeplot()
+comparison.chart = popularity %>%
+  pivot_longer(current:expected, names_to = 'popularity.type', values_to = 'popularity') %>% 
+  ggplot(aes(x = datadatetime, y = popularity, color = popularity.type)) +
+  geom_line() +
+  facet_wrap(. ~ venuename) +
+  xlab('') +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90))
 
 comparison.chart
 
 ggsave('analysis/plot.png', comparison.chart, units = 'in', width = 11, height = 7)
 
-writetime = stamp('2020-03-20 23:30:47')
-
-popularity %>%
-  mutate(
-    datadatetime = writetime(datadatetime)
-  ) %>% 
-  write_csv('analysis/foottraffic-clean.csv')
-
-popularity %>%
-  mutate(
-    datadatetime = writetime(datadatetime),
-    popularity.type = str_c(popularity.type, '.popularity')
-  ) %>% 
-  pivot_wider(names_from = popularity.type, values_from = popularity) %>% 
-  write_csv('analysis/foottraffic-clean-wide.csv')
-
 ind.plots = popularity %>% 
-  mutate(datadatetime = floor_date(datadatetime, unit = 'hour')) %>% 
-  distinct(datadatetime, venuename, typeofspace, popularity.type, .keep_all = TRUE) %>% 
-  pivot_wider(names_from = popularity.type, values_from = popularity) %>% 
   group_by(venuename) %>% 
   nest() %>% 
   mutate(
@@ -66,14 +35,7 @@ ind.plots = popularity %>%
         ggtitle(.y) +
         theme_minimal() +
         theme(axis.text.x = element_text(angle = 90))
-        #+
-        # theme(axis.title.x = element_blank(),
-        #       axis.title.y = element_blank(),
-        #       panel.grid.major.x = element_blank(),
-        #       panel.grid.minor.x = element_blank(),
-        #       panel.grid.minor.y = element_blank())
-    ),
-    # save2 = walk2(plot, venuename, ~ggsave(str_c('analysis/svg/', .y, '-combo.svg'), .x, units = 'in', width = 11, height = 7))
+    )
   )
 
 ind.plots
@@ -81,7 +43,7 @@ ind.plots
 ind.plots %>% 
   mutate(
     save = walk2(
-      plot, venuename, ~ggsave(str_c('analysis/png/', .y, '.png'), .x, units = 'in', width = 11, height = 7)
+      plotobj, venuename, ~ggsave(str_c('analysis/png/', .y, '.png'), .x, units = 'in', width = 11, height = 7)
     )
   )
     
@@ -93,8 +55,7 @@ ind.plots.svg = ind.plots %>%
       'MedMen WeHo',
       'Santa Monica Pier',
       'LACMA',
-      'Dockweiler Beach',
-      'Grand Central Market'
+      'Dockweiler Beach'
     )
   ) %>%
   mutate(
@@ -113,5 +74,4 @@ ind.plots.svg = ind.plots %>%
   )
 
 ind.plots.svg %>% 
-  head() %>% 
-  pull(svgplot)
+  mutate(save = walk2(svgplot, venuename, ~ggsave(str_c('analysis/svg/', .y, '.svg'), .x, units = 'in', width = 11, height = 7)))
